c9024c96ccceb4723176c52533b9c026507797c8
Alex <lolposerk@gmail.com>
2025-11-25 17:00:08 +0300

    create

--- main.js
+++ main.js
@@ -1,44 +1,14 @@
-let hunger = 50;
-let happiness = 50;
-let health = 50;
+const userProfile = {};
 
-let inGame = true
+userProfile.username = 'developer123';
+userProfile.email = 'dev@example.com';
+userProfile.age = 25;
+userProfile.isLoggedIn = true;
 
-while (inGame == true) {
-    let action = prompt(`
-        Ваш питомец:
-        Голод: ${hunger}
-        Счастье: ${happiness}
-        Здоровье: ${health}
-        ---
-        Чем займемся?
-        1. Кормить
-        2. Играть
-        3. Уложить спать
-        4. Выход
-    `);
+console.log(userProfile.email);
 
-    switch (action) {
-        case '1':
-            hunger += 5;
-            happiness -= 1;
-            health -= 1
-            break
-        case '2':
-            hunger -= 5;
-            happiness += 15;
-            health -= 5
-            break
-        case '3':
-            hunger -= 10;
-            happiness -= 3;
-            health = 100
-            break
-        case '4':
-            inGame = false
-    }
+userProfile.age = 26;
 
-    if (hunger >= 100, happiness <= 0, health <= 0) {
-        inGame = false
-    }
-}
+userProfile['user location'] = 'New York';
+
+console.log(userProfile['user location']);
