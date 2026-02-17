9c63cf999c0a10918930bc36676a303285e86365
Alex <lolposerk@gmail.com>
2026-02-17 14:08:43 +0300

    git

--- homework.py
+++ homework.py
@@ -1,40 +1,55 @@
-emails = [
-    "user1@example.com",
-    "user2@example.com", 
-    "user1@example.com",  # Дубликат
-    "user3@example.com",
-    "user2@example.com",  # Дубликат
-    "admin@example.com",
-    "user4@example.com"
-]
+contacts = {}
 
-EmailDuplicates = set(emails)
+def findFromName(name):
+    return contacts.get(name)
+
+def deleteFromName(name):
+    return contacts.pop(name)
+
+def addContact(name,phone):
+    if findFromName(name):
+        print(f"Телефон контакта: '{name}' успено изменён на: '{phone}'.")
+    else:
+        print(f"Контакт '{name}' успешно добавлен!")
+    contacts[name] = phone
+
+print("="*40)
+print()
+
+addContact("Иван","+7-123-456-78-90")
+addContact("Мария","+7-987-654-32-10")
+addContact("Алексей","+7-111-222-33-44")
 
 print()
 print("="*40)
 print()
 
-print(f"Уникальные email: {EmailDuplicates}")
+contactFind = findFromName("Иван")
 
-print()
-
-print(f"Дубликатов было удалено: {len(emails)-len(EmailDuplicates)}")
-
-print()
-
-
-HaveAdminEmail = False
-
-for email in EmailDuplicates:
-    if email == "admin@example.com":
-        HaveAdminEmail = True
-        break
-
-if HaveAdminEmail == True:
-    print(f'email: "admin@example.com" есть в списке')
+if contactFind:
+    print(f"Телефон: '{contactFind}' Найден по имени Иван!")
 else:
-    print(f'email: "admin@example.com" нет в списке')
+    print(f"Телефон не был найден по имени Иван!")
+
+
+contactDel = deleteFromName("Иван")
+print(f"Контакт: 'Иван' Удалён!")
 
 print()
 print("="*40)
 print()
+
+addContact("Алексей","+7-444-333-222-11")
+
+print()
+print("="*40)
+print()
+
+print("Все контакты:")
+print()
+
+for name, phone in contacts.items():
+    print(f"    Имя: '{name}',Телефон: '{phone}'")
+
+print()
+print("="*40)
