3ad00639f8595dd7b87d47bb393d34b0fac20d9c
Alex <lolposerk@gmail.com>
2026-02-17 14:05:13 +0300

    git

--- homework.py
+++ homework.py
@@ -1,39 +1,39 @@
-student1 = ("Иван", "Иванов", 85, 92)
-student2 = ("Мария", "Петрова", 78, 88)
-student3 = ("Алексей", "Сидоров", 95, 90)
+emails = [
+    "user1@example.com",
+    "user2@example.com", 
+    "user1@example.com",  # Дубликат
+    "user3@example.com",
+    "user2@example.com",  # Дубликат
+    "admin@example.com",
+    "user4@example.com"
+]
 
-Students = (student1,student2,student3)
-
-SmartStudent = [0,""]
-
-for student in Students:
-    print()
-    print("="*40)
-    print()
-
-    name, surname, math, physics = student
-
-    print(f"Имя: {name}")
-    print(f"Фамилия: {surname}")
-
-    MiddleGrade = (math + physics) / 2
-
-    if MiddleGrade > SmartStudent[0]:
-        SmartStudent[0] = MiddleGrade
-        SmartStudent[1] = name
-
-    if math >= 60 and physics >= 60:
-        print(f"Сдал(a) все предметы")
-    else:
-        print(f"Не cдал(a) все предметы")
-    
-    print(f"Средняя оценка: {MiddleGrade}")
+EmailDuplicates = set(emails)
 
 print()
 print("="*40)
 print()
 
-print(f"Студент {SmartStudent[1]}, с самым высоким средним балом '{SmartStudent[0]}'")
+print(f"Уникальные email: {EmailDuplicates}")
+
+print()
+
+print(f"Дубликатов было удалено: {len(emails)-len(EmailDuplicates)}")
+
+print()
+
+
+HaveAdminEmail = False
+
+for email in EmailDuplicates:
+    if email == "admin@example.com":
+        HaveAdminEmail = True
+        break
+
+if HaveAdminEmail == True:
+    print(f'email: "admin@example.com" есть в списке')
+else:
+    print(f'email: "admin@example.com" нет в списке')
 
 print()
 print("="*40)
